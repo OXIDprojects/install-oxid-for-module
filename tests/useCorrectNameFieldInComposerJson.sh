ea68c7e66e8500901d2ce6d82b15a9b2cdf50793
alfredbez <alfred.bez@googlemail.com>
2020-01-23 17:02:28 +0100

    copy all files from bin directory to tmp test dir

--- tests/useCorrectNameFieldInComposerJson.sh
+++ tests/useCorrectNameFieldInComposerJson.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-cp "$DIR"/../bin/install-oxid "$DIR"/test-composer-name/
+cp "$DIR"/../bin/* "$DIR"/test-composer-name/
 cd "$DIR"/test-composer-name/ || exit
 
 output=$(bash install-oxid)
